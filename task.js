a4887e71612382cfc5cd3a02518258b8cbe9d260
VanyaPonomarev <70202618+VanyaPonomarev@users.noreply.github.com>
2021-12-21 13:01:56 +0300

    Update task.js

--- task.js
+++ task.js
@@ -21,6 +21,10 @@ Student.prototype.addMark = function (mark) {
 }
 
 Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+        this.marks = [];
+    }
+    
     for (let mark of marks) {
         this.marks.push(mark);
     }
